0860f65563fed8d373a4cfefe1321cdcc2b8366d
Mahdi sayyadi <96723318+Sayyadi68@users.noreply.github.com>
2022-06-07 18:03:24 +0430

    Add files via upload

---
+++ java/Theme.js
@@ -0,0 +1,52 @@
+var checkbox = document.getElementById("ChangeTheme"); //get the checkbox to a variable
+
+//check storage if dark mode was on or off
+if (sessionStorage.getItem("mode") == "dark") {
+  darkmode(); //if dark mode was on, run this funtion
+} else {
+  nodark(); //else run this funtion
+}
+
+//if the checkbox state is changed, run a funtion
+checkbox.addEventListener("change", function() {
+  //check if the checkbox is checked or not
+  if (checkbox.checked) {
+    darkmode(); //if the checkbox is checked, run this funtion
+  } else {
+    nodark(); //else run this funtion
+  }
+});
+
+//function for checkbox when checkbox is checked
+function darkmode() {
+  document.documentElement.setAttribute('data-theme','dark'); //add a class to the body tag
+  checkbox.checked = true; //set checkbox to be checked state
+  sessionStorage.setItem("mode", "dark"); //store a name & value to know that dark mode is on
+}
+
+//function for checkbox when checkbox is not checked
+function nodark() {
+  document.documentElement.setAttribute('data-theme','light'); //remove added class from body tag
+  checkbox.checked = false; //set checkbox to be unchecked state
+  sessionStorage.setItem("mode", "light"); //store a name & value to know that dark mode is off or light mode is on
+}
+
+// document.body.classList.add("class name"); //it will be add a new class
+
+var slideIndex = 1;
+showDivs(slideIndex);
+
+function plusDivs(n) {
+  showDivs(slideIndex += n);
+}
+
+function showDivs(n) {
+  var i;
+  var x = document.getElementsByClassName("slid");
+  if (n > x.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = x.length}
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = "none";  
+  }
+  x[slideIndex-1].style.display = "block";  
+}
